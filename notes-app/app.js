ad7c6f789210a7571a822e79f5531461a07e3ef7
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-15 15:16:58 -0800

    process argument

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,20 @@
+const chalk = require('chalk');
+// const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
-
 console.log(msg);
+
+// using chalk module
+const blueMsg = chalk.blue.inverse.bold('Success!');
+console.log(blueMsg);
+
+// process object
+/* 
+    run: node app.js Alex
+    Output should be the messages you expect above and an array of
+    that contains three different strings, which you can see are paths.
+    Two of them are always provided. 1) The executable to nodejs on your computer
+    2) the path to our app.js file
+*/
+console.log(process.argv[2]);
