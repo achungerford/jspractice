f1a297fca944777e9c5bead13e58c4b0c1d23238
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-15 09:59:19 -0800

    exports and require

---
+++ notes-app/app.js
@@ -0,0 +1,15 @@
+const getNotes = require('./notes.js');
+
+const msg = getNotes();
+
+console.log(msg);
+
+/* challenge 1: see utils.js
+
+Uncomment the lines below to execute the first challenge.
+You will also need to uncommet the lines from utils.js
+*/
+
+// const add = require('./utils.js');
+// const sum = add(4, -2);
+// console.log(sum);
---
+++ notes-app/notes.js
@@ -0,0 +1,6 @@
+const getNotes = function () {
+    return 'Your notes...';
+}
+
+// exports
+module.exports = getNotes;
