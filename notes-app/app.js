8c46f3cf6dc854a34654c8ed6f52793a00764031
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-17 15:20:21 -0800

    handler working properly

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,20 +1,50 @@
+// core, npm, local
 const chalk = require('chalk');
-// const validator = require('validator');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+// customize yargs version
+yargs.version('1.1.0');
 
-// using chalk module
-const blueMsg = chalk.blue.inverse.bold('Success!');
-console.log(blueMsg);
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title'
+        }
+    },
+    handler: function () {
+        console.log('placeholder: Adding a new note!');
+    }
+})
 
-// process object
-/* 
-    run: node app.js Alex
-    Output should be the messages you expect above and an array of
-    that contains three different strings, which you can see are paths.
-    Two of them are always provided. 1) The executable to nodejs on your computer
-    2) the path to our app.js file
-*/
-console.log(process.argv[2]);
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('placeholder: removing the note');
+    }
+})
+
+// Create the list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('placeholder: listing out all the notes');
+    }
+})
+
+// Create the read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function () {
+        console.log('placeholder: reading a note');
+    }
+})
+
+console.log(yargs.argv);
