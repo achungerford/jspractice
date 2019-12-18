27f72448327d7eb0e81dfcb9781d44bb6dee58fb
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-18 15:34:57 -0800

    tutorial challenge vid 18

--- playground/1-json.js
+++ playground/1-json.js
@@ -8,9 +8,33 @@ const fs = require('fs');
 // fs.writeFileSync('1-json.json', bookJSON);
 
 // calling dataBuffer bc return is not a string, but a buffer
-const dataBuffer = fs.readFileSync('1-json.json', );
-const dataJSON = dataBuffer.toString();
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
 
-// parsing our saved dataJSON
-const data = JSON.parse(dataJSON);
-console.log(data.title);
+// // parsing our saved dataJSON
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+
+//
+// --------- tutorial challenge ----------
+// read the data into buffer
+const profileBuffer = fs.readFileSync('1-json.json');
+console.log(profileBuffer);
+// convert buffer to string
+const profileJSON = profileBuffer.toString();
+console.log(profileJSON);
+// parse into object, user
+const user = JSON.parse(profileJSON);
+console.log(user);
+
+// change the data
+user.name = 'Barack'
+user.age = 58
+
+console.log(user);
+// stringify user object
+const userJSON = JSON.stringify(user);
+console.log(userJSON);
+// write the data stored in userJSON string to file called 1-json.json
+fs.writeFileSync('1-json.json', userJSON);
