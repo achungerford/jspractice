5991dda694911e7db0d100f50dfcf5a1bc683135
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-18 12:31:57 -0800

    reading JSON data to buffer

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
-const book = {
-    title: 'America for Americans',
-    author: 'Erika Lee'
-}
+// const book = {
+//     title: 'America for Americans',
+//     author: 'Erika Lee'
+// }
 
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync('1-json.json', bookJSON);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+// calling dataBuffer bc return is not a string, but a buffer
+const dataBuffer = fs.readFileSync('1-json.json', );
