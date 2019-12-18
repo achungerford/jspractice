b507771305ad2d6f67d021d673556dac73f8447b
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-17 20:46:14 -0800

    returning empty array, can add new notes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,20 +7,20 @@ const getNotes = function () {
 // job is to save notes to data store
 const addNote = function (title, body) {
     const notes = loadNotes();
+
+    notes.push({});
+
     console.log(notes);
 }
 
 // function for removing notes
 const loadNotes = function () {
-    // code in case notes.json file not present
+    // if any of 3 cases fail
     try {
         const dataBuffer = fs.readFileSync('notes.json');
-        // convert buffer into string
         const dataJSON = dataBuffer.toString();
-        // parse the string
         return JSON.parse(dataJSON);
     } catch (e) {
-        // if no notes.json
         return []; 
     }
 }
