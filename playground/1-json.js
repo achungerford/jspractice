693e124be01df16d7eb160beb05dae7613009466
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-12-18 12:24:43 -0800

    write JSON to file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,7 @@
+const fs = require('fs');
 const book = {
     title: 'America for Americans',
     author: 'Erika Lee'
 }
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
-
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData.author);
