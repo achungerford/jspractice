4e40b41d11bc480e204f00ba3c7885f13e5e77a8
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-01-09 13:17:05 -0800

    for methods: refactor functions defined on objects

--- notes-app/app.js
+++ notes-app/app.js
@@ -21,7 +21,7 @@ yargs.command({
             type: 'string',
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body);
     }
 })
@@ -37,7 +37,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title);
     }
 })
@@ -46,7 +46,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List the notes',
-    handler: function () {
+    handler() {
         // placeholder
         console.log('Listing out all the notes');
     }
@@ -56,7 +56,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Reads a note',
-    handler: function () {
+    handler() {
         // placeholder
         console.log('placeholder: reading a note');
     }
