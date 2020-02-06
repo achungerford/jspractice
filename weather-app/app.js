a7d396388dd6c959731601e9aaa2e1840a9b8ad4
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-06 11:03:57 -0800

    hide api key

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,2 +1,12 @@
 // require the module we installed
 const request = require('request');
+require('dotenv').config();
+
+const api_key = process.env.API_KEY;
+
+const url = `https://api.darksky.net/forecast/${api_key}/37.8267,-122.4233`;
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.currently);
+});
