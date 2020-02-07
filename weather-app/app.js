bf48862d2f17dc8872109966e315d87d4f792ef7
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-06 23:09:25 -0800

    add json property and comments

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,13 @@
 const request = require('request');
 require('dotenv').config();
 
+// bring in API key from a .env file
 const api_key = process.env.API_KEY;
 
+// using the example API request
 const url = `https://api.darksky.net/forecast/${api_key}/37.8267,-122.4233`;
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+// making an http request 
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.currently);
 });
