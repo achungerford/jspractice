db9bbd18d0b2147126e77f0a46e4b29872b65cbb
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-07 00:31:17 -0800

    incl summary for day

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const api_key = process.env.API_KEY;
 
 // using the example API request
-const url = `https://api.darksky.net/forecast/${api_key}/37.8267,-122.4233?units=si`;
+const url = `https://api.darksky.net/forecast/${api_key}/37.8267,-122.4233`;
 
 // making an http request 
 request({ url: url, json: true }, (error, response) => {
-    console.log('It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
 });
