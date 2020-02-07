631b49a974c42268142083038d1ec29115adf9df
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-06 23:24:22 -0800

    challenge: print temp & rain

--- weather-app/app.js
+++ weather-app/app.js
@@ -10,5 +10,5 @@ const url = `https://api.darksky.net/forecast/${api_key}/37.8267,-122.4233`;
 
 // making an http request 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.currently);
+    console.log('It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
 });
