7195abdf1d834a1fb198629338fd2ee872fc7791
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-15 19:56:16 -0800

    geocode

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,3 +1,13 @@
+// callback pattern - asynchronous
+// setTimout from node API
 setTimeout(() => {
     console.log('Two seconds are up.');
 }, 2000);
+
+const names = ['Andrew', 'Jen', 'Jess'];
+
+// callback pattern - synchronous
+// standard javascript, no api
+const shortNames = names.filter((name) => {
+    return name.length <= 4;
+});
