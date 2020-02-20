8c46adca4ddc26744998c41e7ecc3a1abba7179b
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-20 15:37:28 -0800

    solution to callback challenge

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -39,6 +39,12 @@
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
 
+const add = (x, y, callback) => {
+    setTimeout(() => {
+        callback(x + y);
+    }, 2000);
+}
+
 add(1, 4, (sum) => {
     console.log(sum) // Should print: 5
 })
