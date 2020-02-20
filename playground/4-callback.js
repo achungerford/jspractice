0e60a5670bc7e36507f2c23e7806753f5010f0d5
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-20 15:26:12 -0800

    callback challenge

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,31 +1,44 @@
-// callback pattern - asynchronous
-// setTimout from node API
-setTimeout(() => {
-    console.log('Two seconds are up.');
-}, 2000);
+// // callback pattern - asynchronous
+// // setTimout from node API
+// setTimeout(() => {
+//     console.log('Two seconds are up.');
+// }, 2000);
 
 
-const names = ['Andrew', 'Jen', 'Jess'];
+// const names = ['Andrew', 'Jen', 'Jess'];
 
-// callback pattern - synchronous
-// standard javascript, no api
-const shortNames = names.filter((name) => {
-    return name.length <= 4;
-});
+// // callback pattern - synchronous
+// // standard javascript, no api
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4;
+// });
 
 
-// geocode example
-const geocode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
+// // geocode example
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
     
-        callback(data)
-    }, 2000);
-}
+//         callback(data)
+//     }, 2000);
+// }
 
-geocode('Philadelphia', (firstArgument) => {
-    console.log(firstArgument);
-});
+// geocode('Philadelphia', (firstArgument) => {
+//     console.log(firstArgument);
+// });
+
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
