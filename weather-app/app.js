3ed77c3272ad9fb01670460eab2b04111d28411e
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-20 21:32:41 -0800

    commenting out code to work on new functions

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,33 +6,34 @@ require('dotenv').config();
 const weather_api_key = process.env.WEATHER_API_KEY;
 const map_api_key = process.env.MAP_API_KEY;
 
-// using the example API request
-const url = `https://api.darksky.net/forecast/${weather_api_key}/37.8267,-122.4233`;
+// // using the example API request
+// const url = `https://api.darksky.net/forecast/${weather_api_key}/37.8267,-122.4233`;
 
-// error handling: disconnected wifi
-// making an http request for weather API
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service.');
-    } else if (response.body.error) {
-        console.log('Unable to find location');
-    } else {
-        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
-    }
-});
+// // error handling: disconnected wifi
+// // making an http request for weather API
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service.');
+//     } else if (response.body.error) {
+//         console.log('Unable to find location');
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
+//     }
+// });
 
-// Geocoding using the api to get lattitude & longitude
-const map_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${map_api_key}&limit=1`;
+// // Geocoding using the api to get lattitude & longitude
+// const map_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${map_api_key}&limit=1`;
 
-// http request for map API. Note that geocoordinates for lat/long are usually presented as [longitdue, lattitude].
-request({ url: map_url, json: true } , (error, response) => {
-    if (error) {
-        console.log('Unable to access geolocation services!');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.');
-    } else {
-        const lattitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(response.body.features[0].text + ', lattitude: ' + lattitude + ', longitude: ' + longitude);
-    }
-});
+// // http request for map API. Note that geocoordinates for lat/long are usually presented as [longitdue, lattitude].
+// request({ url: map_url, json: true } , (error, response) => {
+//     if (error) {
+//         console.log('Unable to access geolocation services!');
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search.');
+//     } else {
+//         const lattitude = response.body.features[0].center[1];
+//         const longitude = response.body.features[0].center[0];
+//         console.log(response.body.features[0].text + ', lattitude: ' + lattitude + ', longitude: ' + longitude);
+//     }
+// });
+
