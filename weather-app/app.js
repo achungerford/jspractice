49ff3335bf6c5b234b74f35bc4aff4cbbd4ca4c3
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-21 00:16:39 -0800

    eles if

--- weather-app/app.js
+++ weather-app/app.js
@@ -45,6 +45,8 @@ const geocode = (address, callback) => {
     request({ url: url, json: true, }, (error, response) => {
         if (error) {
             callback('Unable to connect to location services!', undefined);
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined);
         }
     });
 }
