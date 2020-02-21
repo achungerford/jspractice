e809f6d4af122c860e87be695954017395ddbc5c
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-20 23:08:19 -0800

    begin error handler

--- weather-app/app.js
+++ weather-app/app.js
@@ -43,7 +43,9 @@ const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + encodeURIComponent(address) + `.json?access_token=${map_api_key}&limit=1`;
 
     request({ url: url, json: true, }, (error, response) => {
-
+        if (error) {
+            callback();
+        }
     });
 }
 
