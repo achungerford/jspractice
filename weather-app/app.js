21a9e9d6fc03686253eed58b77221b35defbb16c
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-25 01:10:35 -0800

    error handling: unable to connect

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,11 +6,10 @@ require('dotenv').config();
 const weather_api_key = process.env.WEATHER_API_KEY;
 const map_api_key = process.env.MAP_API_KEY;
 
-// // using the example API request
+// // Weather request: Lat/Long -> weather
 // const url = `https://api.darksky.net/forecast/${weather_api_key}/37.8267,-122.4233`;
 
-// // error handling: disconnected wifi
-// // making an http request for weather API
+// // error handling
 // request({ url: url, json: true }, (error, response) => {
 //     if (error) {
 //         console.log('Unable to connect to weather service.');
@@ -21,26 +20,26 @@ const map_api_key = process.env.MAP_API_KEY;
 //     }
 // });
 
-// // Geocoding using the api to get lattitude & longitude
-// const map_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${map_api_key}&limit=1`;
+// // Geocoding request: address -> Lat/Long
+// const map_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${map_api_key}`;
 
-// // http request for map API. Note that geocoordinates for lat/long are usually presented as [longitdue, lattitude].
+// // http request for map API. Note that geocoordinates for lat/long are usually presented as [longitdue, latitude].
 // request({ url: map_url, json: true } , (error, response) => {
 //     if (error) {
 //         console.log('Unable to access geolocation services!');
 //     } else if (response.body.features.length === 0) {
 //         console.log('Unable to find location. Try another search.');
 //     } else {
-//         const lattitude = response.body.features[0].center[1];
 //         const longitude = response.body.features[0].center[0];
-//         console.log(response.body.features[0].text + ', lattitude: ' + lattitude + ', longitude: ' + longitude);
+//         const latitude = response.body.features[0].center[1]; 
+//         console.log(response.body.features[0].text + ', latitude: ' + latitude + ', longitude: ' + longitude);
 //     }
 // });
 
 
 // function definition
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + encodeURIComponent(address) + `.json?access_token=${map_api_key}&limit=1`;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${map_api_key}&limit=1`;
 
     request({ url: url, json: true}, (error, response) => {
         if (error) {
