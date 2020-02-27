7e6ea2b012b69ed712243a77584a8b4fb73e4acd
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-26 18:09:39 -0800

    bug fix

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,6 +11,6 @@ geocode('Kansas City', (error, data) => {
 
 // longitude/latitude -> weather
 forecast(39.0845, -94.563, (error, data) => {
-    console.log('Error:', error)
-    console.log('Data:', data)
+    console.log('Error:', error);
+    console.log('Data:', data);
   });
