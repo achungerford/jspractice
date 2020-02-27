56edb604141930263eed9c8c3d624f778cbcbe11
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-26 17:33:44 -0800

    error handling forecast complete

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -8,15 +8,15 @@ const forecast = (latitude, longitude, callback) => {
     
     const url = `https://api.darksky.net/forecast/${weather_api_key}/${latitude},${longitude}`;
     
-    request({url: url, json: true}, (error, response) => {
+    request({ url: url, json: true }, (error, response) => {
         if (erorr) {
-            
-        } else if (condition) {
-            
+            callback('Unable to connect to weather service!', undefined);
+        } else if (response.body.error) {
+            callback('Unable to find location.', undefined)
         } else {
-            
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
         }
-    })
+    });
 }
 
 
