64dfb00e1e36417dd035577bbbb55ac2ae19406f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-04 15:45:01 -0800

    es6 objects

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,12 @@
+// Object property shorthand
+
+const name = 'John';
+const userAge = 30;
+
+const user = {
+    name: name,
+    age: userAge,
+    location: 'Los Angeles'
+}
+
+console.log(user);
