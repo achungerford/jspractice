07011a368475511560d6adcbbe5c4ce7052c14f6
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-04 15:47:41 -0800

    ojbect shorthand

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -1,10 +1,11 @@
 // Object property shorthand
+// when the value of a property in an object comes from a variable of the same name as the property
 
 const name = 'John';
 const userAge = 30;
 
 const user = {
-    name: name,
+    name,
     age: userAge,
     location: 'Los Angeles'
 }
