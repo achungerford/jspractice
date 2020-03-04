3208ce5a3c028591e9d5b82b11cc81830080bddc
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-04 15:19:52 -0800

    create address variable and use in geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,8 +3,12 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 require('dotenv').config();
 
+const address = process.argv[2];
+
+console.log(process.argv);
+
 // city -> longitude/latitude
-geocode('Boston', (error, data) => {
+geocode(address, (error, data) => {
     if (error) {
         return console.log(error);
     }
