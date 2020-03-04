5a0cf06cea2f2ff2d65c425a601f377296f6028d
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-04 10:35:02 -0800

    error handling geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 
 // city -> longitude/latitude
 geocode('Boston', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-    
+    if (error) {
+        return console.log(error);
+    }
+
     // longitude/latitude -> weather
     forecast(data.latitude, data.longitude, (error, data) => {
         console.log('Error:', error);
