ccf38ce306e99a1aebf1413e2e2e2da7f94fc36f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-04 19:28:25 -0800

    destructure response in geocode

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -7,16 +7,16 @@ const map_api_key = process.env.MAP_API_KEY;
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${map_api_key}`;
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined);
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined);
         } else {
             callback(undefined, {
-                longitude: response.body.features[0].center[0],
-                latitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                location: body.features[0].place_name
             });
         }
     });
