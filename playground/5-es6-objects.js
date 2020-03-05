d2c6cc47ddf710a9715cace42cc3b56c632da939
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-04 17:21:42 -0800

    destructuring: properties not on object

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -11,3 +11,25 @@ const user = {
 }
 
 console.log(user);
+
+// Object destructuring
+// To extract property values of an object into individual varialbes.
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    qty: 201,
+    salePrice: undefined 
+}
+
+// old way
+// const label = product.label;
+
+// es6 way - inside {}, list properties you want to extract
+// when destructuring, you can even include properties that don't yet exist on the object, like rating
+const {label, qty, rating} = product;
+
+console.log(label);
+console.log(qty);
+
+// you can even destructure
