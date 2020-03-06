2791eda26ee0b19a289b92ee05668c004fff925f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-06 10:53:35 -0800

    response() set up

---
+++ playground/6-raw-http.js
@@ -0,0 +1,30 @@
+/* Understanding what the 'request' package is doing by
+recreating one of our HTTP requests.
+*/
+
+const https = require('https');
+const url = `www.someURLhere.com`;
+
+// instead of response.body.etc like we get with the npm 'request' package,
+// we have to "listen" for each part of the HTTPS response to come in because
+// parts of HTTPS requests can arrive at different times
+// -- think pages of a letter in separate envelopes (packets)
+// bc the letter won't make sense until you receive all the pages
+
+// must listen for each individual chunk to arrive
+// must listen for when all chunks have finally arrived
+
+https.request(url, (response) => {
+
+    // response.on allows us to register a handler
+    // event name = first argument = 'data' here
+    response.on('data', (chunk) => {
+
+    });
+
+    // figure out when we're done
+    // event name = 'end'; the callback gets no arguments, just by running we know its done
+    response.on('end', () => {
+
+    });
+});
