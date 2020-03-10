fc2fd5326ac8ac4daf15a5d418c4a4e308031c7e
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-09 23:19:53 -0700

    rendering JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -23,7 +23,10 @@ app.get('/', (req, res) => {
 
 // create a help route
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send({
+        name: 'Obama',
+        president: 44
+    });
 });
 
 // create an about route
