374292e2c6c9275b960721403c1b6e5385fe267d
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-09 23:03:06 -0700

    removed comments

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,55 +1,42 @@
 /* creating our nodejs script that will
 create, configure, and start up the server.
 
-We're going to load in Express, configure it to serve something up,
-and then start the server
-
-This script is the starting point to our node applicatoin.
+run script: node src/app.js
+keep server running: nodemon src/app.js
 */
 
-// load in library
+// load in library, store application to variable
 const express = require('express');
-
-// variable to store our express application. Express only exposes a single function.
 const app = express();
 
-/* express() doesn't take any arguments.
-We configure our server using *methods* provided on the application itself.
-
-Imagine we have a single domain 'app.com' and all of it is going to run on a
-single Express server, but we'll have multiple routes set up.
--- root route (app.com)
--- slash help (app.com/help)
--- slash about (app.com/about)
+/*
+root route (app.com)
+help route (app.com/help)
+about route (app.com/about)
+weather route (app.com/weather)
 */
 
-// set up server to send a response when someone tries to get something at a specific route
-// req is an object that contains info about the incoming request to the server
-
-// create root page
+// create root route
 app.get('/', (req, res) => {
-    res.send('Hello Express!');     // displays in browser
+    res.send('Hello Express!');
 });
 
 // create a help route
 app.get('/help', (req, res) => {
-    res.send('Help page');     // displays in browser
+    res.send('Help page');
 });
 
 // create an about route
 app.get('/about', (req, res) => {
-    res.send('About page');     // displays in browser
+    res.send('About page');
 });
 
 // create a weather route
 app.get('/weather', (req, res) => {
-    res.send('Weather page');     // displays in browser
+    res.send('Weather page');
 });
 
-// code to start server - port 3000 is common development port
+// port 3000 is common development port, starts server
 app.listen(3000, () => {
     console.log('Server is up on port 3000');   // never displays in browser
 });
-
-// run script: node src/app.js
-// keep server running: nodemon src/app.js
