0b34b80f5b530eddfedfdb64f3e682dd0c376f63
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-09 19:18:31 -0700

    new script to create, configure, and start up server

---
+++ web-server/src/app.js
@@ -0,0 +1,9 @@
+/* creating our nodejs script that will
+create, configure, and start up the server.
+
+We're going to load in Express, configure it to serve something up,
+and then start the server
+
+This script is the starting point to our node applicatoin.
+*/
+
