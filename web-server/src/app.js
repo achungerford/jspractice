d0f6eb15d28aa74f315366180d9beb5d7a5ef1f6
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-14 13:40:34 -0700

    removed routes 'about' and 'help'

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -13,20 +13,6 @@ const publicDirectoryPath = path.join(__dirname, '../public');  // absolute path
 // serve 'public' directory
 app.use(express.static(publicDirectoryPath));
 
-// create a help route
-app.get('/help', (req, res) => {
-    res.send([
-        {name: 'Barack H. Obama'},
-        {name: 'George W. Bush'},
-        {name: 'William J. Clinton'}
-    ]);
-});
-
-// create an about route
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-});
-
 // create a weather route
 app.get('/weather', (req, res) => {
     res.send({
