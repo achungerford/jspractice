3c9dd75d04f266877f2eee3c7ef1daa5987d5f51
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-24 19:43:48 -0700

    use fetchAPI to get data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client-side javascript file is loaded.');
+
+// this code can't be used in a back-end node script
+// the 'Fetch API' is not part of javascript. It is a browser-based API for modern browsers.
+
+// since this script is running in client-side JS, using the Fetch API is fine here
+
+fetch('http://puzzle.mead.io/puzzle').then(response) => {
+    response.json().then((data) => {
+        console.log(data);
+    });
+}
